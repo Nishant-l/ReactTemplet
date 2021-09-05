528db17409be064c1c4b164e40c8d79647ed0f94
Nishant-l <nishant3151998@gmail.com>
2021-09-05 19:15:08 +0530

    General React Templet;

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './app/index.js',
+  module: {
+    rules: [
+      { test: /\.svg$/, use: 'svg-inline-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      { test: /\.(js)$/, use: 'babel-loader' }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+}
